f0af2f03a3468a7d06a656f08f3737d6457c6118
Wooksang Ryu <today8934@gmail.com>
2024-09-22 21:52:40 +0900

    feature: Movie App Part One

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response =
+        await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+    console.log(json);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {loading ?
+            <h1>Loading...</h1> :
+            <div>
+              {movies.map(movie =>
+                  <div key={movie.id}>
+                    <img src={movie.medium_cover_image} alt={movie.title} />
+                    <h2>{movie.title}</h2>
+                    <p>{movie.summary}</p>
+                    {movie.hasOwnProperty('genres') ?
+                        <ul>
+                          {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+                        </ul>
+                        : null
+                    }
+                  </div>
+              )}
+            </div>
+        }
+      </div>
   );
 }
 
